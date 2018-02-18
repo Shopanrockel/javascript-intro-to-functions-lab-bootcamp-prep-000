804267024279c6e756b43737c23f284fa4a4d452
Skip Harris <skip.harris@barclaycollege.edu>
2018-02-18 21:48:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   
   return "I can\'t hear you!" 
 }
-function sayHiToGrandma(string) {
+function sayHiToGrandma("HELLO") {
   var uppercase = "HELLO"
   uppercase.toUpperCase() === uppercase
   
